383f3b83b7523c3f3344d12ac415cba7a34a1d5c
EvilEntity <me@piotrjastrzebski.io>
2016-01-26 22:36:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hi!
+
+This is my artemis + libgdx quickstart for jams.
+
+A bunch of base classes for getting stuff to work asap!
---
+++ core/src/io/piotrjastrzebski/tastyjam/Box2dUtils.java
@@ -0,0 +1,12 @@
+package io.piotrjastrzebski.tastyjam;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by EvilEntity on 26/01/2016.
+ */
+public class Box2dUtils {
+	public void doBox2dStuff() {
+		Gdx.app.log("", "Doing stuff, i swear!");
+	}
+}
