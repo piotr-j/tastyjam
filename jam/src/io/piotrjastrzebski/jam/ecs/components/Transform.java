9f745e7419bb7df9a49fe2f5f4b33812e54319ba
piotr-j <me@piotrjastrzebski.io>
2016-03-29 22:46:02 +0200

    added cxy setter

--- jam/src/io/piotrjastrzebski/jam/ecs/components/Transform.java
+++ jam/src/io/piotrjastrzebski/jam/ecs/components/Transform.java
@@ -36,6 +36,14 @@ public class Transform extends PooledComponent {
 		return this;
 	}
 
+	public Transform cxy (float cx, float cy) {
+		this.x = cx - width / 2;
+		this.y = cy - height / 2;
+		this.cx = cx;
+		this.cy = cy;
+		return this;
+	}
+
 	public Transform cxy (Vector2 centre) {
 		this.x = centre.x - width / 2;
 		this.y = centre.y - height / 2;
